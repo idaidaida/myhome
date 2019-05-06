30d4cf501ea2f388c6c7445df4cb6478fcd3606b
yuhei-ida <yuuhei0311>
2019-05-06 19:08:54 +0900

    アカウント設定時にデフォルトでアイコンが選択されるように修正しました

--- app/controllers/configs_controller.rb
+++ app/controllers/configs_controller.rb
@@ -5,6 +5,7 @@ class ConfigsController < ApplicationController
         @account_baseinfo_form = AccountBaseinfoForm.new
         @account_baseinfo_form.account_name = login_account_obj.account_name
         @account_baseinfo_form.display_name = login_account_obj.display_name
+        @account_baseinfo_form.icon = login_account_obj.icon
         @accounts = Account.all
     end
 
